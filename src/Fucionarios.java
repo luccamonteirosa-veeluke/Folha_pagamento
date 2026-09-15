aaab5a1e6fdfe8f85a96cdada496b9d491b16787
Lucca monteiro De sá <lucca.monteiro.sa@gmail.com>
2026-09-15 14:20:27 -0300

    added payroll system

--- src/Fucionarios.java
+++ src/Fucionarios.java
@@ -1,7 +1,7 @@
 public abstract class Funcionario {
     protected String nome;
     protected String cpf;
-    protected double salario;
+    protected double salario; 
 
     public Funcionario(String nome, String cpf, double salario) {
         this.nome = nome;
